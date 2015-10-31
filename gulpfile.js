b0432d59a7c1c6503fda9bef791b0c6429a192d6
Shikkic <Shikkic@gmail.com>
2015-10-31 15:50:52 -0400

    added gulp to run a build and run task

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    exec = require('child_process').exec,
+    gutil = require('gulp-util');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('build', function(cb) {
+    exec('git update-index --assume-unchanged ./.env', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        gutil.log(gutil.colors.green(".env will now be ignored locally."), gutil.colors.red("NEVER COMMIT YOUR .ENV PUBLICLY!"));
+        cb(err);
+    }); 
+});
+
+gulp.task('run', function(cb) {
+    gutil.log(gutil.colors.magenta("Running hackerTracker9000"));
+    var child = exec('node index.js');
+    child.stdout.on('data', function(data) {
+        console.log('stdout: ' + data);
+    });
+    child.stderr.on('data', function(data) {
+        console.log('stdout: ' + data);
+    });
+    child.on('close', function(code) {
+        console.log('closing code: ' + code);
+    });
+});
