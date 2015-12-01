d5acf5027204824b9670d3f6a894aab1d17c40db
Dan Cadden <shikkic@gmail.com>
2015-12-01 16:04:26 -0500

    test

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
 
 gulp.task('build', function(cb) {
